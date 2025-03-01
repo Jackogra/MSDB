e419ef98109ef2cc692c4463e565e2815e01c3f9
Jacek Gralka <jackogra@tlen.pl>
2025-03-02 00:21:11 +0100

    Add Series number formatting, add str methods

--- MSDB.py
+++ MSDB.py
@@ -11,13 +11,23 @@ class Movie:
     def play(self):
         self.play_count += 1
 
+    def __str__(self):
+        return f"{self.title} ({self.release_year})"
+
 
 class Series(Movie):
     def __init__(self, title, release_year, genre, season, episode):
         super().__init__(title, release_year, genre)
-        self.season = season
-        self.episode = episode
+        self.season = format(season, "02d")
+        self.episode = format(episode, "02d")
         self.play_count = 0
 
     def play(self):
         self.play_count += 1
+
+    def __str__(self):
+        return f"{self.title} S{self.season}E{self.episode}"
+
+
+nowy = Movie("The Simpsons", 1998, "Comedy")
+print(nowy.__str__())
